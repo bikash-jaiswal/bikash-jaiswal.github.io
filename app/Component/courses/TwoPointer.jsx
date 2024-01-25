066698a0df7667dbeb7efcbdfd88246b37e5305d
Bikash Jaiswal <bjjaiswal@gmail.com>
2024-01-25 00:16:40 -0500

    update two pointer in course

---
+++ app/Component/courses/TwoPointer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const TwoPointer = () => {
+    return (
+        <div> 
+            <div>The Two Pointers pattern is a technique commonly used in computer programming and algorithms. It involves maintaining two pointers (indexes or references) that traverse a data structure, typically an array or a linked list, to solve a problem more efficiently. The two pointers move towards each other, converging or diverging based on the problem requirements.
+            </div>
+            <div className='p-2'>There are two main variations of the Two Pointers pattern:</div>
+            <ol className='list-disc ml-6'>
+                <li>Two Pointers Approach:
+                    <ol className='list-disc ml-6'>
+                        <li> <b>Usage:</b> To efficiently solve problems with linear data structures like arrays or linked lists.</li>
+                        <li><b>How it works: </b>Use two pointers, one starting from the beginning and the other from the end, or both starting from the same end but moving at different rates. </li>
+                        <li><b>Example: </b>Finding a pair of elements in a sorted array that sum up to a target value. </li>
+                    </ol>
+                </li>
+                <li>Fast and Slow Pointers:
+                    <ol className='list-disc ml-6'>
+                        <li> <b>Usage:</b> For problems involving linked lists or arrays where you need to find a specific position or cycle.</li>
+                        <li><b>How it works: </b>Use two pointers, one moving faster than the other. The faster pointer might move by two or more steps at a time.</li>
+                        <li><b>Example: </b>Detecting a cycle in a linked list.</li>
+                    </ol>
+                </li>
+                 <li>Modified Binary Search: Introduction:
+                    <div className='p-2'>Modified Binary Search refers to variations or adjustments made to the traditional binary search algorithm to solve specific types of problems. While the traditional binary search is used to find a specific element in a sorted array, modified versions are applied to solve problems where a binary search-like approach can be beneficial. Here's a general idea of how Modified Binary Search works and an example to illustrate its usage:</div>
+                    <ol className='list-disc ml-6'>
+                        <li> <b>Usage:</b> Modified Binary Search is employed to efficiently find the first or last occurrence of a target element in a sorted array, adapting the standard binary search for specific requirements.</li>
+                        <li><b>How it works: </b>The algorithm maintains pointers and adjusts them based on comparisons with the target element, ensuring a focused search in either the left or right half of the sorted array to locate the desired occurrence efficiently.</li>
+                        <li><b>Example: </b>Finding the Square Root using Modified Binary Search.</li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
+    )
+}
+
+export default TwoPointer
--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,22 +1,13 @@
 import React, { ReactNode } from 'react';
-
+import TwoPointer from '../Component/courses/TwoPointer';
 
 const faqData = [
     {
       question: "Two Pointers Patterns",
-      answer: (
-        <>
-          <div>All business we take up carry the following guarantees:</div>
-          <ul className="list-disc ml-6">
-            <li>Privacy Guarantee: All client details will be kept confidential.</li>
-            <li>Price Guarantee: Minimum half cost-saving over on-site development.</li>
-            <li>Result Guarantee: Progress-linked instalments weighted at the end.</li>
-          </ul>
-        </>
-      ),
+      answer: <TwoPointer />,
     },
     {
-        question: "In-place Reversal of a Linked List, Stacks,",
+        question: "Linked List, Stacks, Queue, Matrices",
         answer: "Yes, code paper agrees to deliver your project within a predetermined due date."
     },
     {
@@ -24,24 +15,36 @@ const faqData = [
         answer: "Our designers are committed to delivering good quality code. We have formal procedures to keep quality under tight control. Every project goes through the hands of appropriate QA/Testing experts, underserve test plans including black-box testing, white-box testing, test regression, usage analysis etc."
     },
     {
-        question: "Dynamic Programming:",
+        question: "Trees, DFS and BFS",
         answer: "Yes, that developer will work full-time (8Hours a day, 6 days a week) dedicating it only to you."
     },
     {
-        question: "Backtracking:",
+        question: "Sliding windows",
         answer: "Yes, we provide technical and maintenance services on request."
     },
     {
-        question: "Would you replace my developer if I am not satisfied?",
+        question: "Hashmap, Heaps",
+        answer: "Yes, we provide technical and maintenance services on request."
+    },
+    {
+        question: "Merge Interval Patterns",
         answer: "Yes, we will positively replace your developer if we see that there is a deficiency on the developer’s end."
     },
     {
-        question: "Will my hired developers speak English?",
+        question: "Subsets and Backtracking",
         answer: "Yes, all our developers are speaking English fluently."
     },
     {
-        question: "Can I contact the appropriate parties working on our project? If so can I communicate via phone?",
-        answer: "Sure, We would suggest that you interact primarily with the team leader to minimize miscommunications with your team. We are alright with correspondence through telephone, E-mail, or skype."
+        question: "Dynamic Programming pattern",
+        answer: "Sure, "
+    },
+     {
+        question: "Graph, Trie and Custom Data Structures",
+        answer: "Sure, "
+    },
+     {
+        question: "Bit manipulation, Union Find",
+        answer: "Sure, "
     }
     
   ];
